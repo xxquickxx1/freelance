472057de1c32035cc1bce3cee31b2f1d0cad0e26
Thomas <thomas@thomas-VirtualBox.(none)>
2015-06-27 20:49:24 +0100

    hehe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Freelance::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
+config.assets.compile = ['*.js', '*.css']
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
@@ -21,7 +21,7 @@ Freelance::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
